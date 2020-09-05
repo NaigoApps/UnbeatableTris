ab816d80b213ce8001cefa9c6a72c71b39b784cb
Lorenzo Niccolai <lorenzo.niccolai94@gmail.com>
2020-09-06 00:18:50 +0200

    avanzamento

--- index.js
+++ index.js
@@ -1,101 +1,110 @@
+const EMPTY = {
+  value: 0,
+  css: "",
+};
+const X = {
+  value: 1,
+  css: "X",
+  score = +1
+};
+const O = {
+  value: 2,
+  css: "O",
+  score = -1
+};
+
 const board = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
+  [EMPTY.value, EMPTY.value, EMPTY.value],
+  [EMPTY.value, EMPTY.value, EMPTY.value],
+  [EMPTY.value, EMPTY.value, EMPTY.value]
 ];
 
-function init(){
-    document.getElementById("0").onclick = () => putX(0);
-    document.getElementById("1").onclick = () => putX(1);
-    document.getElementById("2").onclick = () => putX(2);
-    document.getElementById("3").onclick = () => putX(3);
-    document.getElementById("4").onclick = () => putX(4);
-    document.getElementById("5").onclick = () => putX(5);
-    document.getElementById("6").onclick = () => putX(6);
-    document.getElementById("7").onclick = () => putX(7);
-    document.getElementById("8").onclick = () => putX(8);
+function init() {
+  for (let i = 0; i < 9; i += 1) {
+    $("#" + i).click(() => putX(i));
+  }
 }
 
-function putX(id){
-    const row = Math.floor(id / board.length);
-    const col = Math.floor(id % board[row].length);
-    board[row][col] = 1;
+function putX(id) {
+  const row = Math.floor(id / board.length);
+  const col = Math.floor(id % board[row].length);
+  board[row][col] = 1;
 
-    cpu();
+  cpu();
 
-    draw();
+  draw();
 }
 
-function cpu(){
-    //Calcolo "intelligente"
-
-    //board[?][?] = 2;
+function cpu() {
+  //Calcolo "intelligente"
+  //board[?][?] = 2;
 }
 
-function draw(){
-    for(let r = 0;r < board.length;r++){
-        for(let c = 0;c < board[r].length;c++){
-            const id = (r * board.length + c).toString();
-            if(board[r][c] === 1){
-                document.getElementById(id).classList = "box X"
-            }else if(board[r][c] === 2){
-                document.getElementById(id).classList = "box O"
-            }else {
-                document.getElementById(id).classList = "box"
-            }
-        }
+function draw() {
+  for (let r = 0; r < board.length; r++) {
+    for (let c = 0; c < board[r].length; c++) {
+      const id = (r * board.length + c).toString();
+      const component = $("#" + id);
+      component.removeClass(X.css);
+      component.removeClass(O.css);
+      if (board[r][c] === 1) {
+        component.addClass(X.css);
+      } else if (board[r][c] === 2) {
+        component.addClass(O.css);
+      }
     }
+  }
 }
 
-function minMax(max){
-
-        if(isFull()){
-            //TODO
-        }
-
-        for(let r = 0;r < board.length;r++){
-            for(let c = 0;c < board[r].length;c++){
-                if(board[r][c] === 0){
-                    
-                }
-            }
-        }
-
-
-        for(int i = 0;i < moves.size();i++){
-            eaterCell = config[(moves[i].startId & ROW) / 8][moves[i].startId & COLUMN];
-            //Cancello le pedine mangiate
-            config[(moves[i].startId & ROW) / 8][moves[i].startId & COLUMN] = NORMAL | NOBODY;
+function minMax(symbol) {
+  const availableMoves = [];
+  for (let r = 0; r < board.length; r++) {
+    for (let c = 0; c < board[r].length; c++) {
+      if (board[r][c] === EMPTY.value) {
+        availableMoves.push({
+          row: r,
+          column: c,
+        });
+      }
+    }
+  }
+  let current = -1;
+  let max = -1;
+  for(let m = 0;m < availableMoves.length;m++){
     
-            res = minMax(config,level-1,moves[i],!max);
-            if(max && res.value > best.value){
-                best = moves[i];
-                best.value = res.value;
-            }
-            if(!max && res.value < best.value){
-                best = moves[i];
-                best.value = res.value;
-            }
-        }
-        if(moves.size() == 0){
-            arrived.value = (max)?-1000:1000;
-            return arrived;
-        }
-    
-        return best;
+  }
+
+  for (let i = 0; i < moves.size(); i++) {
+    eaterCell = config[(moves[i].startId & ROW) / 8][moves[i].startId & COLUMN];
+    //Cancello le pedine mangiate
+    config[(moves[i].startId & ROW) / 8][moves[i].startId & COLUMN] =
+      NORMAL | NOBODY;
+
+    res = minMax(config, level - 1, moves[i], !max);
+    if (max && res.value > best.value) {
+      best = moves[i];
+      best.value = res.value;
     }
+    if (!max && res.value < best.value) {
+      best = moves[i];
+      best.value = res.value;
+    }
+  }
+  if (moves.size() == 0) {
+    arrived.value = max ? -1000 : 1000;
+    return arrived;
+  }
 
-
-
+  return best;
 }
 
-function isFull(){
-    for(let r = 0;r < board.length;r++){
-        for(let c = 0;c < board[r].length;c++){
-            if(board[r][c] === 0){
-                return false;
-            }
-        }
+function isFull() {
+  for (let r = 0; r < board.length; r++) {
+    for (let c = 0; c < board[r].length; c++) {
+      if (board[r][c] === 0) {
+        return false;
+      }
     }
-    return true;
+  }
+  return true;
 }
